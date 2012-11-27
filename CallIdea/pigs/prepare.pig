3de55e760dcef7056ecfb03230ba8571fd189ea2
dmitrdv <dmitrdv@mail.ru>
2012-11-27 21:02:55 +0600

    рассчет индексов номеров, чтение файла из hdfs

--- CallIdea/pigs/prepare.pig
+++ CallIdea/pigs/prepare.pig
@@ -9,10 +9,15 @@ bnumbers = FOREACH calls GENERATE BNumber as Number;
 allnumbers = UNION anumbers, bnumbers;
 
 numbers = DISTINCT allnumbers;
+numbers_index = FOREACH numbers GENERATE Number, AutoIncrement() as NumberIndex;
+
+calls_tmp1 = JOIN calls BY ANumber, numbers_index BY Number;
+calls_tmp2 = JOIN calls_tmp1 BY BNumber, numbers_index BY Number;
+calls_index = FOREACH calls_tmp2 GENERATE $0 as BNumber, $1 as ANumber, $3 as ANumberIndex, $5 as BNumberIndex;
 
 numbers_group = GROUP numbers ALL;
 numbers_count = FOREACH numbers_group GENERATE COUNT(numbers);
 
-STORE calls INTO '/user/hadoop/res/calls' USING PigStorage('\t');
+STORE calls_index INTO '/user/hadoop/res/calls' USING PigStorage('\t');
 STORE numbers_count INTO '/user/hadoop/res/count' USING PigStorage('\t');
-STORE numbers INTO '/user/hadoop/res/numbers' USING PigStorage('\t');
+STORE numbers_index INTO '/user/hadoop/res/numbers' USING PigStorage('\t');
--- CallIdea/src/Main.java
+++ CallIdea/src/Main.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.pig.ExecType;
@@ -16,13 +22,15 @@ public class Main {
 	    String path = StringUtils.strip(file.getPath(), "file:");
 		
 		HdfsServiceImpl hdfsAdminService = new HdfsServiceImpl();
-		FileSystem hdfs = hdfsAdminService.getFileSystem();
+		FileSystem hdfs = hdfsAdminService.getFileSystem();				
+		
 	    try {
 			hdfs.copyFromLocalFile(false, true, new Path(path), new Path("/user/hadoop/cdr/"));
 			hdfs.delete(new Path("/user/hadoop/res"), true);
 
 			PigServer pigServer = new PigServer(ExecType.MAPREDUCE);
 			pigServer.registerJar("C:/git/CallIdea/BAnalysis/bin/BAnalysis.jar");
+			pigServer.registerJar("C:/git/CallIdea/AutoIncrement/bin/AutoIncrement.jar");
 			pigServer.registerScript("C:/git/CallIdea/CallIdea/pigs/prepare.pig");			
 		} catch (ExecException e) {
 			// TODO Auto-generated catch block
@@ -30,7 +38,35 @@ public class Main {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}	   	    
+	    
+	    hdfsAdminService.close();
 	}
+	
+	  private static void validateResult(HdfsService hdfsAdminService, String dir) {
+		    FileStatus[] fsStatus = hdfsAdminService.list(dir);
+		    if(fsStatus == null || fsStatus.length == 0) return;
+
+		    for (FileStatus status : fsStatus) {
+		      if ( status.isDir() || !status.getPath().toString().startsWith("part") ) {
+		        continue;
+		      }
+		       
+		      List<String> actual = hdfsAdminService.read(new HdfsService.ReadCallback<List<String>>() {
+
+		        @Override
+		        public List<String> read(BufferedReader reader) throws IOException {
+		        	List<String> results = new ArrayList<String>();
+		          String line = null;
+		          while ((line = reader.readLine()) != null) {
+		            results.add(line);
+		          }
+		          return results;
+		        }
+		        
+		      }, status.getPath());
+		      System.out.println("Reduced Map:" + actual);
+		    }
+		  }
 		  
 }
